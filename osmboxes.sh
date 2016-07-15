54a47153ecde9e30033ada09cf1e2cd0d04ef8ef
Santiago Crespo <github.com@flanera.net>
2016-07-15 21:11:20 +0200

    Update osmboxes.sh

--- osmboxes.sh
+++ osmboxes.sh
@@ -9,13 +9,11 @@ perl -pe 's/{"geo/\n{"geo/g' $2 | grep '"state": 0' > /tmp/tareas
 NAME=`echo $1 | awk -F '.' '{print $1}'`
 
 while IFS='' read -r linea || [[ -n "$linea" ]]; do
-
-echo $linea > /tmp/linea
+	echo $linea > /tmp/linea
 	X=`awk -F '"x": ' '{print $2}' /tmp/linea | awk -F ',' '{print $1}'`
 	Y=`awk -F '"y": ' '{print $2}' /tmp/linea | awk -F ',' '{print $1}'`
-    perl -pe 's/\[\[\[\[/XXXXX\n/' /tmp/linea | perl -pe 's/\]\]\]\]/\nXXXX/g' | grep -v XXXX | tr -d '[],' | perl -pe 's/ /\n/g' | sort -nu | perl -pe 's/\n/,/g' > /tmp/coordenadas
+	perl -pe 's/\[\[\[\[/XXXXX\n/' /tmp/linea | perl -pe 's/\]\]\]\]/\nXXXX/g' | grep -v XXXX | tr -d '[],' | perl -pe 's/ /\n/g' | sort -nu | perl -pe 's/\n/,/g' > /tmp/coordenadas
 	BOX=`awk -F ',' '{print $1","$3","$2","$4}' /tmp/coordenadas`
-
 	FILE=$NAME-$X-$Y.osm
 echo $FILE
 	osmconvert $1 -b=$BOX -o=$FILE
