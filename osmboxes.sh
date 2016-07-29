c2d5f8c9e12f639c971469fe0aaf99bc49225617
Santiago Crespo <github.com@flanera.net>
2016-07-29 18:46:37 +0200

    Update osmboxes.sh

--- osmboxes.sh
+++ osmboxes.sh
@@ -10,8 +10,8 @@ NAME=`echo $1 | awk -F '.' '{print $1}'`
 
 while IFS='' read -r linea || [[ -n "$linea" ]]; do
 	echo $linea > /tmp/linea
-	X=`awk -F '"x": ' '{print $2}' /tmp/linea | awk -F ',' '{print $1}'`
-	Y=`awk -F '"y": ' '{print $2}' /tmp/linea | awk -F ',' '{print $1}'`
+	X=`awk -F '"x": ' '{print $2}' /tmp/linea | awk -F ',' '{print $1}'` | tr -d '}'
+	Y=`awk -F '"y": ' '{print $2}' /tmp/linea | awk -F ',' '{print $1}'` | tr -d '}'
 	perl -pe 's/\[\[\[\[/XXXXX\n/' /tmp/linea | perl -pe 's/\]\]\]\]/\nXXXX/g' | grep -v XXXX | tr -d '[],' | perl -pe 's/ /\n/g' | sort -nu | perl -pe 's/\n/,/g' > /tmp/coordenadas
 	BOX=`awk -F ',' '{print $1","$3","$2","$4}' /tmp/coordenadas`
 	FILE=$NAME-$X-$Y.osm
